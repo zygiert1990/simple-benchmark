8d4cb2c3e99a4ab9287e843baac44684de401ce2
Michał Zyga <36279820+zygiert1990@users.noreply.github.com>
2026-02-01 06:29:02 +0100

    using CountDownLatch instead of busy loop & add possibility to override log to check and app name

--- src/main/java/org/zygiert/Main.java
+++ src/main/java/org/zygiert/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,31 +17,30 @@ public class Main {
         ExecutorService terminationExecutor = Executors.newSingleThreadExecutor();
 
         for (int i = 0; i < parameters.measureIterations; i++) {
+	        CountDownLatch latch = new CountDownLatch(1);
             long startTime = System.currentTimeMillis();
             Process process = processBuilder.start();
-            ProcessWatcher processWatcher = new ProcessWatcher(process.getInputStream());
+            ProcessWatcher processWatcher = new ProcessWatcher(process.getInputStream(), parameters.startLog, latch);
             executor.execute(processWatcher);
-            while (true) {
-                if (processWatcher.serverStarted) {
-                    long elapsedTime = System.currentTimeMillis() - startTime;
-                    System.out.println("Elapsed time: " + elapsedTime + " ms");
-                    if (parameters.isNativeImage) {
-                        process.destroy();
-                        Thread.sleep(5000); // for unknown reason I have to wait here, perhaps for release some resources on OS, otherwise I get strange results
-                    } else {
-                        killJVMProcess(terminationExecutor);
-                    }
-                    break;
-                }
+            
+            latch.await();
+
+	        long elapsedTime = System.currentTimeMillis() - startTime;
+            System.out.println("Elapsed time: " + elapsedTime + " ms");
+            if (parameters.isNativeImage) {
+                process.destroy();
+                Thread.sleep(5000); // for unknown reason I have to wait here, perhaps for release some resources on OS, otherwise I get strange results
+            } else {
+                killJVMProcess(terminationExecutor, parameters.appName);
             }
         }
 
         System.exit(0);
     }
 
-    private static void killJVMProcess(ExecutorService executor) throws IOException, InterruptedException, ExecutionException {
+    private static void killJVMProcess(ExecutorService executor, String appName) throws IOException, InterruptedException, ExecutionException {
         Process killProcess = new ProcessBuilder()
-                .command("sh", "-c", "kill -9 `jps | grep 'simple-application' | awk '{print $1}'`")
+                .command("sh", "-c", String.format("kill -9 `jps | grep '%s' | awk '{print $1}'`", appName))
                 .start();
         DefaultProcessListener defaultProcessListener = new DefaultProcessListener(killProcess.getInputStream());
         executor.submit(defaultProcessListener).get();
@@ -57,31 +57,38 @@ public class Main {
         private final int measureIterations;
         private final String command;
         private final boolean isNativeImage;
+	    private final String startLog;
+	    private final String appName;
 
         private Parameters(String[] args) {
-            if (args.length != 2) {
-                throw new IllegalArgumentException("Invalid number of arguments! You must specify two arguments (measureIterations, command)!");
+            if (args.length < 2) {
+                throw new IllegalArgumentException("Invalid number of arguments! You must specify at least two arguments (measureIterations, command)!");
             }
             measureIterations = Integer.parseInt(args[0]);
             command = args[1];
             isNativeImage = !command.contains("java");
+	        startLog = (args.length > 2) ? args[2] : "Server started";
+	        appName = (args.length > 3) ? args[3] : "simple-application";
         }
     }
 
     private static final class ProcessWatcher implements Runnable {
         private final InputStream inputStream;
-        private volatile boolean serverStarted = false;
+	    private final String startLog;
+        private final CountDownLatch latch;
 
-        private ProcessWatcher(InputStream inputStream) {
+        private ProcessWatcher(InputStream inputStream, String startLog, CountDownLatch latch) {
             this.inputStream = inputStream;
+	        this.startLog = startLog;
+	        this.latch = latch;
         }
 
         @Override
         public void run() {
             new BufferedReader(new InputStreamReader(inputStream)).lines()
                     .forEach(line -> {
-                                if (line.contains("Server started")) {
-                                    serverStarted = true;
+                                if (line.contains(startLog)) {
+                                    latch.countDown();
                                 }
                             }
                     );
